6853ca9bca63b8db257aa36c7c3e2f23daa81bae
TarKz06 <khematat_s@elearning.cmu.ac.th>
2021-10-27 00:22:42 +0700

    Create userCard in component and Crate UserList in view dir

---
+++ src/components/UserCard.vue
@@ -0,0 +1,44 @@
+<template>
+  <q-card class="my-card">
+    <q-img :src="users.profileimg">
+      <div class="absolute-bottom text-subtitle2 text-center">
+        <router-link :to="{ name: 'userInfo', params: { id: users.id } }">
+          {{ users.firstname }}
+          {{ users.lastname }}
+        </router-link>
+        <h2>gender : {{users.gender}}</h2>
+        <h2>howtown : {{users.hometown}}</h2>
+      </div>
+    </q-img>
+  </q-card>
+</template>
+
+<script>
+export default {
+  name: 'UserCard',
+  props: {
+    users: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+<style scoped>
+.my-card {
+  width: 350px;
+}
+a:link {
+  color: rgb(233, 230, 230);
+  padding: 15px 25px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+}
+a:visited {
+  color: rgb(70, 70, 70);
+}
+a:active {
+  background-color: rgb(246, 241, 231);
+}
+</style>
--- src/views/User.vue
+++ src/views/User.vue
@@ -1,4 +1,150 @@
 <template>
-    <h1>WELCOME USER</h1>
-    <h3>Please waite for your role.</h3>
+  <h1 class="text">User List</h1>
+  <q-page-container class="flex flex-center q-pa-md row q-gutter-lg">
+    <UserCard v-for="users in userss" :key="users.id" :users="users" />
+  </q-page-container>
+  <div class="users">
+    <div class="pagination">
+      <router-link
+        id="page-prev"
+        :to="{ name: 'User', query: { page: page - 1 } }"
+        rel="prev"
+        v-if="page != 1"
+      >
+        <q-btn style="padding: 15px" color="secondary" label="Prev Page" />
+        <br
+      /></router-link>
+
+      <router-link
+        id="page-next"
+        :to="{ name: 'User', query: { page: page + 1 } }"
+        rel="next"
+        v-if="hasNextPage"
+      >
+        <q-btn style="padding: 15px" color="secondary" label="Next Page" />
+        <br
+      /></router-link>
+    </div>
+    <div class="test2"></div>
+  </div>
 </template>
+
+<script>
+// @ is an alias to /src
+import UserCard from '@/components/UserCard.vue'
+import UserService from '@/services/UserService.js'
+
+export default {
+  name: 'User',
+  props: {
+    page: {
+      type: Number,
+      required: true
+    }
+  },
+  components: {
+    UserCard // register it as a child component
+  },
+  data() {
+    return {
+      userss: null,
+      totalUsers: 0,
+      keyword: null
+    }
+  },
+  // eslint-disable-next-line no-unused-vars
+  beforeRouteEnter(routeTo, routeFrom, next) {
+    UserService
+      .getEvents(10, parseInt(routeTo.query.page) || 1)
+      .then((response) => {
+        next((comp) => {
+          comp.userss = response.data
+          comp.totalUsers = response.headers['x-total-count'] // <--- Store it
+        })
+      })
+      .catch(() => {
+        next({ name: 'NetworkError' })
+      })
+  },
+  beforeRouteUpdate(routeTo) {
+    var queryFunction
+    if (this.keyword == null || this.keyword === '') {
+      queryFunction = UserService.getEvents(
+        5,
+        parseInt(routeTo.query.page) || 1
+      )
+    } else {
+      queryFunction = UserService.getEventByKeyword(
+        this.keyword,
+        5,
+        parseInt(routeTo.query.page) || 1
+      )
+    }
+      
+    queryFunction
+      .then((response) => {
+        this.userss = response.data
+        this.totalUsers = response.headers['x-total-count']
+      })
+      .catch(() => {
+        return{ name: 'NetworkError' }
+      })
+  },
+  methods: {
+    updateKeyword() {
+      var queryFunction
+      if (this.keyword === '') {
+        queryFunction = UserService.getEvents(5, 1)
+      } else {
+        queryFunction = UserService.getEventByKeyword(this.keyword, 5, 1)
+      }
+
+      queryFunction
+        .then((response) => {
+          this.userss = response.data
+          console.log(this.events)
+          this.totalUsers = response.headers['x-total-count']
+          console.log(this.totalUsers)
+        })
+        .catch(() => {
+          return { name: 'NetworkError' }
+        })
+    }
+  },
+  computed: {
+    hasNextPage() {
+      let totalPages = Math.ceil(this.totalUsers / 5)
+      return this.page < totalPages
+    }
+  }
+}
+</script>
+<style scoped>
+.users {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.pagination {
+  display: flex;
+  width: 500px;
+}
+.pagination a {
+  flex: 1;
+  text-decoration: none;
+  color: #2c3e50;
+}
+#page-prev {
+  text-align: left;
+}
+#page-next {
+  text-align: right;
+}
+.test {
+  font-family: 'Courier New', monospace;
+}
+.test2 {
+  font-family: 'Courier New', monospace;
+  padding: 70px;
+}
+</style>
